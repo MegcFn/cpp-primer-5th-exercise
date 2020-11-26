2b10ba1614c2ceabf54beb3cce6309a79a1f4dd9
MegcFn <38220847+MegcFn@users.noreply.github.com>
2020-11-27 01:36:09 +0900

    Update Exercise1_9.cpp

--- Chapter1/Section1_4_1/Exercise1_9.cpp
+++ Chapter1/Section1_4_1/Exercise1_9.cpp
@@ -3,10 +3,13 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    int sum = 0;
-    for (int val = 50; val <= 100;val++) {
+    int sum = 0, val = 50;
+
+    while (val<=100){
         sum += val;
+        val++;
     }
     std::cout << "The sum is " << sum << std::endl;
     return 0;
 }
+
